d1878038b4ddee5bb256559466e7c79fdc54d7c3
405 <405@xzl.com>
2023-12-10 15:48:28 +0800

    2023-12-10 15:48:28

---
+++ project/dome/build/CMakeFiles/dome.dir/DependInfo.cmake
@@ -0,0 +1,19 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/aurora/ganhuo/C-and-CPP/project/dome/dome.cpp" "CMakeFiles/dome.dir/dome.cpp.o" "gcc" "CMakeFiles/dome.dir/dome.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ project/dome/build/CMakeFiles/dome.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for dome.
+# This may be replaced when dependencies are built.
